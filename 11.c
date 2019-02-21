ba113a1aeb944ac158e3893eb01026e7407dd4bb
Divyarrr <45899415+Divyarrr@users.noreply.github.com>
2019-02-21 15:48:56 +0530

    11.c

---
+++ 11.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+    char a[100];
+    scanf("%s",a);
+    int i;
+    for(i=0;a[i]!='\0';i++)
+    {
+         if(a[0]=='s')
+         {
+
+printf("yes");
+break;
+}
+else
+    {
+        printf("no");
+    }
+}}
